b82a33f243cf528d53e040662d324a3b78a27f6c
tsurumeso <nstm101339@gmail.com>
2017-06-03 22:47:49 +0900

    Pep8

--- smlr/SMLRsubfunc.py
+++ smlr/SMLRsubfunc.py
@@ -5,98 +5,106 @@
 import numpy
 import scipy
 
-def funcE(theta,alpha,Y,X):
-    #theta: weights for classification, dimensions by # of classes
-    #alpha: relavance parameters in ARD, dimensions by # of classes
-    #Y: label matrix, 1-of-K representation, # of samples by # of classes
-    #X: feature matrix, # of samples by # of dimensions
-    #output: log likelihood function of theta (averaged over alpha)
-    
-    theta=numpy.double(theta)
-    alpha=numpy.double(alpha)
-    X=numpy.double(X)
-    
-    N=X.shape[0]
-    D=X.shape[1]
-    C=Y.shape[1]
-    E=0
-    
-    linearSum=numpy.zeros((N,C))
+
+def funcE(theta, alpha, Y, X):
+    # theta: weights for classification, dimensions by # of classes
+    # alpha: relavance parameters in ARD, dimensions by # of classes
+    # Y: label matrix, 1-of-K representation, # of samples by # of classes
+    # X: feature matrix, # of samples by # of dimensions
+    # output: log likelihood function of theta (averaged over alpha)
+
+    theta = numpy.double(theta)
+    alpha = numpy.double(alpha)
+    X = numpy.double(X)
+
+    N = X.shape[0]
+    D = X.shape[1]
+    C = Y.shape[1]
+    E = 0
+
+    linearSum = numpy.zeros((N, C))
     for c in range(C):
-        linearSum[:,c]=numpy.dot(X,theta[:,c])
-    fone=numpy.sum(Y*linearSum,axis=1)-numpy.log(numpy.sum(numpy.exp(linearSum),axis=1))
-    E=numpy.sum(fone)-(0.5)*numpy.sum(numpy.reshape(theta,(C*D,1),order='F')*((numpy.reshape(alpha,(C*D,1),order='F'))**2))
+        linearSum[:, c] = numpy.dot(X, theta[:, c])
+    fone = numpy.sum(Y * linearSum, axis=1) - \
+        numpy.log(numpy.sum(numpy.exp(linearSum), axis=1))
+    E = numpy.sum(fone) - (0.5) * numpy.sum(numpy.reshape(theta, (C * D, 1), order='F') *
+                                            ((numpy.reshape(alpha, (C * D, 1), order='F'))**2))
     return E
 
 # <codecell>
 
-def gradE(theta,alpha,Y,X):
-    #theta: weights for classification, dimensions by # of classes
-    #alpha: relavance parameters in ARD, dimensions by # of classes
-    #Y: label matrix, 1-of-K representation, # of samples by # of classes
-    #X: feature matrix, # of samples by # of dimensions
-    #output: The gragient of funcE. This is used for Q-step optimization.
-    
-    theta=numpy.double(theta)
-    alpha=numpy.double(alpha)
-    X=numpy.double(X)
-    N=X.shape[0]
-    D=X.shape[1]
-    C=Y.shape[1]
-    
-    dE=numpy.zeros((theta.shape[0]*theta.shape[1],1))
-    linearSum=numpy.zeros((N,C))
-    p=numpy.zeros((N,C))
+
+def gradE(theta, alpha, Y, X):
+    # theta: weights for classification, dimensions by # of classes
+    # alpha: relavance parameters in ARD, dimensions by # of classes
+    # Y: label matrix, 1-of-K representation, # of samples by # of classes
+    # X: feature matrix, # of samples by # of dimensions
+    # output: The gragient of funcE. This is used for Q-step optimization.
+
+    theta = numpy.double(theta)
+    alpha = numpy.double(alpha)
+    X = numpy.double(X)
+    N = X.shape[0]
+    D = X.shape[1]
+    C = Y.shape[1]
+
+    dE = numpy.zeros((theta.shape[0] * theta.shape[1], 1))
+    linearSum = numpy.zeros((N, C))
+    p = numpy.zeros((N, C))
     for c in range(C):
-        linearSum[:,c]=numpy.dot(X,theta[:,c])
+        linearSum[:, c] = numpy.dot(X, theta[:, c])
     for n in range(N):
-        p[n,:]=numpy.exp(linearSum[n,:])/numpy.sum(numpy.exp(linearSum[n,:]))
-    
+        p[n, :] = numpy.exp(linearSum[n, :]) / \
+            numpy.sum(numpy.exp(linearSum[n, :]))
+
     for c in range(C):
-        temporal_dE=numpy.zeros((D,1))
+        temporal_dE = numpy.zeros((D, 1))
         for n in range(N):
-            temporal_dE=temporal_dE+(Y[n,c]-p[n,c])*numpy.transpose([X[n,:]])
-          
-        A=numpy.diag(alpha[:,c])
-        temporal_dE=temporal_dE-numpy.transpose([numpy.dot(A,theta[:,c])])
-        dE[c*D:((c+1)*D),0]=numpy.squeeze(temporal_dE)
+            temporal_dE = temporal_dE + \
+                (Y[n, c] - p[n, c]) * numpy.transpose([X[n, :]])
+
+        A = numpy.diag(alpha[:, c])
+        temporal_dE = temporal_dE - \
+            numpy.transpose([numpy.dot(A, theta[:, c])])
+        dE[c * D:((c + 1) * D), 0] = numpy.squeeze(temporal_dE)
     return numpy.squeeze(dE)
 
 # <codecell>
 
-def HessE(theta,alpha,Y,X):
-    #theta: weights for classification, dimensions by # of classes
-    #alpha: relavance parameters in ARD, dimensions by # of classes
-    #Y: label matrix, 1-of-K representation, # of samples by # of classes
-    #X: feature matrix, # of samples by # of dimensions
-    #output: The Hessian of funcE. This is used for Q-step optimization.
 
-    theta=numpy.double(theta)
-    alpha=numpy.double(alpha)
-    X=numpy.double(X)
-    N=X.shape[0]
-    D=X.shape[1]
-    C=Y.shape[1]
-    
-    
-    linearSum=numpy.zeros((N,C))
-    p=numpy.zeros((N,C))
+def HessE(theta, alpha, Y, X):
+    # theta: weights for classification, dimensions by # of classes
+    # alpha: relavance parameters in ARD, dimensions by # of classes
+    # Y: label matrix, 1-of-K representation, # of samples by # of classes
+    # X: feature matrix, # of samples by # of dimensions
+    # output: The Hessian of funcE. This is used for Q-step optimization.
+
+    theta = numpy.double(theta)
+    alpha = numpy.double(alpha)
+    X = numpy.double(X)
+    N = X.shape[0]
+    D = X.shape[1]
+    C = Y.shape[1]
+
+    linearSum = numpy.zeros((N, C))
+    p = numpy.zeros((N, C))
     for c in range(C):
-        linearSum[:,c]=numpy.dot(X,theta[:,c])
+        linearSum[:, c] = numpy.dot(X, theta[:, c])
 
     for n in range(N):
-        p[n,:]=numpy.exp(linearSum[n,:])/numpy.sum(numpy.exp(linearSum[n,:]))
-    H=numpy.zeros((C*D,C*D))
-    
+        p[n, :] = numpy.exp(linearSum[n, :]) / \
+            numpy.sum(numpy.exp(linearSum[n, :]))
+    H = numpy.zeros((C * D, C * D))
+
     for n in range(N):
-        M1=numpy.diag(p[n,:])
-        M2=numpy.dot(numpy.transpose([p[n,:]]),[p[n,:]])
-        M3=numpy.dot(numpy.transpose([X[n,:]]),[X[n,:]])
-        H=H+numpy.kron(M1-M2,M3)
-    H=-H
-    H=H-numpy.diag(numpy.reshape(alpha,(C*D),order='F'))
-    #Derivation came from Yamashita et al., Nuroimage,2008., but
-    #exactly speaking, the last term is modified.
-    #The above is consistent with SLR toolbox (MATLAB-based implementation by Yamashita-san) rather than the paper.
+        M1 = numpy.diag(p[n, :])
+        M2 = numpy.dot(numpy.transpose([p[n, :]]), [p[n, :]])
+        M3 = numpy.dot(numpy.transpose([X[n, :]]), [X[n, :]])
+        H = H + numpy.kron(M1 - M2, M3)
+    H = -H
+    H = H - numpy.diag(numpy.reshape(alpha, (C * D), order='F'))
+    # Derivation came from Yamashita et al., Nuroimage,2008., but
+    # exactly speaking, the last term is modified.
+    # The above is consistent with SLR toolbox (MATLAB-based implementation by
+    # Yamashita-san) rather than the paper.
     return H
-
--- smlr/SMLRupdate.py
+++ smlr/SMLRupdate.py
@@ -7,104 +7,118 @@ import numpy
 import scipy
 import scipy.optimize
 from smlr import SMLRsubfunc
-def thetaStep(theta,alpha,Y,X,isEffective):
-    
-    #chack # of dimensions, # of samples, and # of classes
-    N=X.shape[0]
-    D=X.shape[1]
-    C=Y.shape[1]
-    
-    #Take indices for effective features (if alpha > 10^3, that dimension is ignored in the following optimization steps)
-    FeatureNum_effectiveWeight=[]
-    ClassNum_effectiveWeight=[]
+
+
+def thetaStep(theta, alpha, Y, X, isEffective):
+
+    # chack # of dimensions, # of samples, and # of classes
+    N = X.shape[0]
+    D = X.shape[1]
+    C = Y.shape[1]
+
+    # Take indices for effective features (if alpha > 10^3, that dimension is
+    # ignored in the following optimization steps)
+    FeatureNum_effectiveWeight = []
+    ClassNum_effectiveWeight = []
     for c in range(C):
         for d in range(D):
-            if isEffective[d,c] == 1:
+            if isEffective[d, c] == 1:
                 FeatureNum_effectiveWeight.append(d)
                 ClassNum_effectiveWeight.append(c)
-    
-    #Declaration of subfunction. this function transform concatenated effective weight paramters into the original shape
+
+    # Declaration of subfunction. this function transform concatenated
+    # effective weight paramters into the original shape
     def thetaConcatenated2thetaOriginalShape(theta_concatenated):
-        if len(theta_concatenated)!=len(FeatureNum_effectiveWeight):
+        if len(theta_concatenated) != len(FeatureNum_effectiveWeight):
             print("the size of theta_concatenated is wrong")
             import sys
             sys.exit()
-        
-        theta_original=numpy.zeros((D,C))
-        for index_effective_weight in range(len(FeatureNum_effectiveWeight)):
-            theta_original[FeatureNum_effectiveWeight[index_effective_weight],ClassNum_effectiveWeight[index_effective_weight]]=\
-                theta_concatenated[index_effective_weight]
-        
-        return theta_original
-    
-    
-    #set the cost function that will be minimized in the following optimization
-    def func2minimize(theta_concatenated):
-        theta_originalShape=thetaConcatenated2thetaOriginalShape(theta_concatenated)
-        return -SMLRsubfunc.funcE(theta_originalShape,alpha,Y,X)
-    
-    #set the gradient for Newton-CG based optimization
-    def grad2minimize(theta_concatenated):
-        theta_originalShape=thetaConcatenated2thetaOriginalShape(theta_concatenated)
-        gradE_originalShape=SMLRsubfunc.gradE(theta_originalShape,alpha,Y,X)
-        
-        #ignore the dimensions that have large alphas
-        dim_ignored=numpy.reshape(isEffective,(C*D,1),order='F')
-        dim_ignored=numpy.nonzero(1-dim_ignored)
-        gradE_used=numpy.delete(gradE_originalShape,dim_ignored[0])
-        return -gradE_used
-    #set the Hessian for Newton-CG based optimization
-    def Hess2minimize(theta_concatenated):
-        theta_originalShape=thetaConcatenated2thetaOriginalShape(theta_concatenated)
-        HessE_originalShape=SMLRsubfunc.HessE(theta_originalShape,alpha,Y,X)
-        
-        #ignore the dimensions that have large alphas
-        dim_ignored=numpy.reshape(isEffective,(C*D,1),order='F')
-        dim_ignored=numpy.nonzero(1-dim_ignored)
-        HessE_used=numpy.delete(HessE_originalShape,dim_ignored[0],axis=0)
-        HessE_used=numpy.delete(HessE_used,dim_ignored[0],axis=1)
-        return -HessE_used
-    
-    
-    #set the initial value for optimization. we use the current theta for this.
-    x0=numpy.reshape(theta,(C*D,1),order='F')
-    dim_ignored=numpy.reshape(isEffective,(C*D,1),order='F')
-    dim_ignored=numpy.nonzero(1-dim_ignored)
-    x0=numpy.delete(x0,dim_ignored[0])
-    
-    #Optimization of theta (weight paramter) with scipy.optimize.minimize
-    import scipy.optimize
-    res = scipy.optimize.minimize(func2minimize, x0, method='Newton-CG',jac=grad2minimize,hess=Hess2minimize,tol=10**(-3))
-    mu=thetaConcatenated2thetaOriginalShape(res['x'])
-    
-    #The covariance matrix of the posterior distribution
-    cov=numpy.linalg.inv(Hess2minimize(res['x']))
 
-    #The diagonal elements of the above covariance matrix
-    var=numpy.diag(cov)
-    var=thetaConcatenated2thetaOriginalShape(var)
-    
-    param={'mu' : mu , 'var' : var}
+        theta_original = numpy.zeros((D, C))
+        for index_effective_weight in range(len(FeatureNum_effectiveWeight)):
+            theta_original[
+                FeatureNum_effectiveWeight[index_effective_weight],
+                ClassNum_effectiveWeight[index_effective_weight]] =\
+                theta_concatenated[index_effective_weight]
+
+        return theta_original
+
+    # set the cost function that will be minimized in the following 
+    # optimization
+    def func2minimize(theta_concatenated):
+        theta_originalShape = thetaConcatenated2thetaOriginalShape(
+            theta_concatenated)
+        return -SMLRsubfunc.funcE(theta_originalShape, alpha, Y, X)
+
+    # set the gradient for Newton-CG based optimization
+    def grad2minimize(theta_concatenated):
+        theta_originalShape = thetaConcatenated2thetaOriginalShape(
+            theta_concatenated)
+        gradE_originalShape = SMLRsubfunc.gradE(
+            theta_originalShape, alpha, Y, X)
+
+        # ignore the dimensions that have large alphas
+        dim_ignored = numpy.reshape(isEffective, (C * D, 1), order='F')
+        dim_ignored = numpy.nonzero(1 - dim_ignored)
+        gradE_used = numpy.delete(gradE_originalShape, dim_ignored[0])
+        return -gradE_used
+    # set the Hessian for Newton-CG based optimization
+
+    def Hess2minimize(theta_concatenated):
+        theta_originalShape = thetaConcatenated2thetaOriginalShape(
+            theta_concatenated)
+        HessE_originalShape = SMLRsubfunc.HessE(
+            theta_originalShape, alpha, Y, X)
+
+        # ignore the dimensions that have large alphas
+        dim_ignored = numpy.reshape(isEffective, (C * D, 1), order='F')
+        dim_ignored = numpy.nonzero(1 - dim_ignored)
+        HessE_used = numpy.delete(HessE_originalShape, dim_ignored[0], axis=0)
+        HessE_used = numpy.delete(HessE_used, dim_ignored[0], axis=1)
+        return -HessE_used
+
+    # set the initial value for optimization. we use the current theta for 
+    # this.
+    x0 = numpy.reshape(theta, (C * D, 1), order='F')
+    dim_ignored = numpy.reshape(isEffective, (C * D, 1), order='F')
+    dim_ignored = numpy.nonzero(1 - dim_ignored)
+    x0 = numpy.delete(x0, dim_ignored[0])
+
+    # Optimization of theta (weight paramter) with scipy.optimize.minimize
+    import scipy.optimize
+    res = scipy.optimize.minimize(
+        func2minimize, x0, method='Newton-CG',
+        jac=grad2minimize, hess=Hess2minimize, tol=10**(-3))
+    mu = thetaConcatenated2thetaOriginalShape(res['x'])
+
+    # The covariance matrix of the posterior distribution
+    cov = numpy.linalg.inv(Hess2minimize(res['x']))
+
+    # The diagonal elements of the above covariance matrix
+    var = numpy.diag(cov)
+    var = thetaConcatenated2thetaOriginalShape(var)
+
+    param = {'mu': mu, 'var': var}
     return param
 # <codecell>
 
 
 # <codecell>
 
-def alphaStep(alpha,theta,var,isEffective):
-    
-    #change the type of input values i
-    theta=numpy.double(theta)
-    alpha=numpy.double(alpha)
-    var=numpy.double(var)
-    D=alpha.shape[0]
-    C=alpha.shape[1]
-    newAlpha=alpha
+def alphaStep(alpha, theta, var, isEffective):
+
+    # change the type of input values i
+    theta = numpy.double(theta)
+    alpha = numpy.double(alpha)
+    var = numpy.double(var)
+    D = alpha.shape[0]
+    C = alpha.shape[1]
+    newAlpha = alpha
     for c in range(C):
         for d in range(D):
-            if isEffective[d,c] == 1:
-                newAlpha[d,c]=(1-alpha[d,c]*var[d,c])/(theta[d,c]**2)
+            if isEffective[d, c] == 1:
+                newAlpha[d, c] = (1 - alpha[d, c] * var[d, c]
+                                  ) / (theta[d, c]**2)
             else:
-                newAlpha[d,c]=10**8
+                newAlpha[d, c] = 10**8
     return newAlpha
-
